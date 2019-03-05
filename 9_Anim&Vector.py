d764d94d3cde4bcf6cd900e45b58c5d5b3fb3435
frostime <frostime@qq.com>
2019-03-05 15:34:00 +0800

    Update to Animation

--- 9_Anim&Vector.py
+++ 9_Anim&Vector.py
@@ -1,49 +1,65 @@
-import numpy as np
+"""
+这节还是讲动画，不涉及新的模块，而是讲述如何使用向量来描述复杂运动
+完成功能：鼠标引导物体移动的功能
+我看的教程里向量用的是一个gamesobjects库，然而那个库用conda安装不了，我又不喜欢它的API，所以用numpy自己写了一个Vecotr类，在vector.py中
+"""
+from vector import Vector
 from sys import exit
 from pygame.locals import *
 import pygame
-background_image_filename = r'./Image/sushiplate.jpg'
-sprite_image_filename = r'./Image/fugu.png'
+import random
 
 
-pygame.init()
+class StarSkyBackground(object):
+    """绘制星空背景图"""
+    def __init__(self, screenSize):
+        w, h = screenSize[0], screenSize[1]
+        self.backcolor = (6, 15, 58)
+        starNum = random.randint(500, 1000)
+        self.stars = []
+        self.starcolor = (200, 200, 200)
+        for i in range(starNum):
+            x = random.randint(0, w)
+            y = random.randint(0, h)
+            self.stars.append((x, y))
 
-screen = pygame.display.set_mode((640, 480), 0, 32)
+    def draw(self, screen):
+        screen.fill(self.backcolor)
+        for star in self.stars:
+            screen.set_at(star, self.starcolor)
 
-background = pygame.image.load(background_image_filename).convert()
-sprite = pygame.image.load(sprite_image_filename).convert_alpha()
 
-clock = pygame.time.Clock()
+def run(FPS=60, v=50):
+    pygame.init()
+    screenSize = (720, 560)
+    screen = pygame.display.set_mode(screenSize)
+    background = StarSkyBackground(screenSize)
+    ship = pygame.image.load(r'./Image/playership.png').convert_alpha()
+    clock = pygame.time.Clock()
+    # 飞船位置
+    position = Vector([100.0, 100.0])
+    # 运动速度大小
+    velocity = v
+    while True:
+        timePassed = clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+        background.draw(screen)
+        screen.blit(ship, position)
+        # 鼠标位置
+        mouse = Vector(pygame.mouse.get_pos())
+        # ship信息
+        shipSize = ship.get_size()
+        # ship的中心位置
+        shipPos = position + Vector(shipSize) / 2
+        # 运动方向，用单位向量描述
+        toward = (mouse - shipPos).normalize()
+        # 速度向量
+        v = toward * velocity
+        print('{}---->{}, V = {}'.format(shipPos, mouse, v))
+        position += v * timePassed / 1000
+        pygame.display.update()
 
-sprite_pos = np.array([200, 150])
-sprite_speed = 300.
-
-while True:
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-
-    pressed_keys = pygame.key.get_pressed()
-
-    key_direction = np.array([0, 0])
-    if pressed_keys[K_LEFT]:
-        key_direction[0] = -1
-    elif pressed_keys[K_RIGHT]:
-        key_direction[0] = +1
-    if pressed_keys[K_UP]:
-        key_direction[1] = -1
-    elif pressed_keys[K_DOWN]:
-        key_direction[1] = +1
-
-    key_direction.normalize()
-
-    screen.blit(background, (0, 0))
-    screen.blit(sprite, sprite_pos)
-
-    time_passed = clock.tick(30)
-    time_passed_seconds = time_passed / 1000.0
-
-    sprite_pos += key_direction * sprite_speed * time_passed_seconds
-
-    pygame.display.update()
+run(v=100)
--- vector.py
+++ vector.py
@@ -8,17 +8,22 @@ class Vector(np.ndarray):
     def __new__(cls, input_array):
         obj = np.asarray(input_array, dtype=float).view(cls)
         obj.dim = len(input_array)
-        if obj.dim <= 3:
-            obj.x = input_array[0]
-            obj.y = input_array[1]
-            if obj.dim == 3:
-                obj.z = input_array[2]
         return obj
 
     def normalize(self):
         norm = self.norm()
-        return self / norm
+        if norm != 0:
+            return self / norm
+        else:
+            return norm.copy()
 
     def norm(self):
         norm = np.linalg.norm(self)
         return norm
+
+    def __str__(self):
+        s = []
+        for ele in self:
+            v = '{:.2f}'.format(ele)
+            s.append(v)
+        return '(' + ', '.join(s) + ')'
